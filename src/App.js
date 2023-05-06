d9e83059cf73098caaaa4e0f7af8ddcb581665df
keishi.i <gajyumaru1103@gmail.com>
2023-05-06 13:04:17 +0900

    reactとwebAPIを使ってpixabayクローンを作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
+import ImageGrallery from './ImageGrallery';
 
 function App() {
+  const [fetchData, setFetchData] = useState([]);
+  const ref = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(ref.current.value);
+
+    //APIURL
+    const endpointURL = `https://pixabay.com/api/?key=31920664-39c530b4998a41dc4209e5bc5&q=${ref.current.value}&image_type=photo`
+    //APIを叩く(データフェッチング)
+    fetch(endpointURL)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data.hits);
+        setFetchData(data.hits);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>My Pixabay</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder="画像を探す" ref={ref} />
+      </form>
+      <ImageGrallery fetchData={fetchData} />
     </div>
   );
 }
